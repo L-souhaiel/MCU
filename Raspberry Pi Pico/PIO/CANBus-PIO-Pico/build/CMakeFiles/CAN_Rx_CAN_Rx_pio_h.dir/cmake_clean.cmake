4f1344d6c2fa04a0e5e7d4ed745d2e5dc8a682d2
TitiLouati <souhaillouati469@yahoo.com>
2021-10-21 21:19:16 +0200

    Submodule

--- Raspberry Pi Pico/PIO/CANBus-PIO-Pico/build/CMakeFiles/CAN_Rx_CAN_Rx_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CAN_Rx.pio.h"
-  "CMakeFiles/CAN_Rx_CAN_Rx_pio_h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CAN_Rx_CAN_Rx_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
